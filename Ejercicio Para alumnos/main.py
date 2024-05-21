a31527190dbc74bc19cb3b196d8f8b750a94333f
Alanelcapon <allahauaakbar@gmail.com>
2024-05-21 12:32:22 -0300

    .

--- Ejercicio Para alumnos/main.py
+++ Ejercicio Para alumnos/main.py
@@ -1,5 +1,8 @@
 from Grafico import *
 from os import *
+import sys
+sys.path.append("Ejercicios")
+from Package_Input.Input import *
 
 def inicio_programa():
     system("cls")
@@ -13,23 +16,23 @@ def inicio_programa():
                 match que_figura:
                     case "a":
                         system("cls")
-                        radio = int(input("Ingrese el radio del círculo:\n"))
+                        radio = get_int("Ingrese el radio del círculo:\n", "Debe ser un número válido", 0, inf, inf)
                         figura = {"tipo_figura": "Circulo",
                                 "dimensiones": (radio,),
                                 "posicion": (1000/2 - radio/2, 850/2 - radio/2 - 100),
                                 "color": seleccionar_color("Ingrese el color del círculo: ")}
                     case "b":
                         system("cls")
-                        base = int(input("Ingrese la base del rectángulo:\n"))
-                        altura = int(input("Ingrese la altura del rectángulo:\n"))
+                        base = get_int("Ingrese la base del rectángulo:\n", "Debe ser un número válido", 0, inf, inf)
+                        altura = get_int("Ingrese la altura del rectángulo:\n", "Debe ser un número válido", 0, inf, inf)
                         figura = {"tipo_figura": "Rectangulo",
                                 "dimensiones": (base, altura),
                                 "posicion": (1000/2 - base/2, 850/2 - altura/2 - 100),
                                 "color": seleccionar_color("Ingrese el color del rectángulo: ")}
                     case "c":
                         system("cls")
-                        base = int(input("Ingrese la base del triángulo:\n"))
-                        altura = int(input("Ingrese la altura del triángulo:\n"))
+                        base = get_int("Ingrese la base del triángulo:\n", "Debe ser un número válido", 0, inf, inf)
+                        altura = get_int("Ingrese la altura del triángulo:\n", "Debe ser un número válido", 0, inf, inf)
                         figura = {"tipo_figura": "Triangulo",
                                 "dimensiones": (base, altura),
                                 "posicion": (1000/2 - base/2, 850/2 - altura/2 - 100),
